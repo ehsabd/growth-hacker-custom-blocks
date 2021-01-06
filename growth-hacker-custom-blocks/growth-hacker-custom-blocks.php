c4880412a2b35d14bfd84fe1d1788a3fdca33f2b
ehsabd <ehsabd@outlook.com>
2021-01-06 17:14:41 +0330

    initial

---
+++ growth-hacker-custom-blocks/growth-hacker-custom-blocks.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+Plugin Name:  Growth Hacker Custom Blocks
+Description:  A Plugin that adds custom blocks to your gutenberg
+Version:      0.1
+Author:       ehsabd
+Text Domain:  growth-hacker-custom-blocks
+Domain Path:  /languages
+*/
+
+function growth_hacker_register_block() {
+ 
+    // automatically load dependencies and version
+    $asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
+ 
+    wp_register_script(
+        'growth-hacker-custom-blocks-xfeature-block',
+        plugins_url( 'build/index.js', __FILE__ ),
+        $asset_file['dependencies'],
+        $asset_file['version']
+    );
+
+    wp_register_style(
+        'growth-hacker-custom-blocks-xfeature-block-editor',
+        plugins_url( 'editor.css', __FILE__ ),
+        array( 'wp-edit-blocks' ),
+        filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' )
+    );
+ 
+    wp_register_style(
+        'growth-hacker-custom-blocks-xfeature-block',
+        plugins_url( 'style.css', __FILE__ ),
+        array( ),
+        filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
+    );
+
+ 
+    register_block_type( 'growth-hacker-custom-blocks/xfeature-block', array(
+        'apiVersion' => 2,
+        'style' => 'growth-hacker-custom-blocks-xfeature-block',
+        'editor_style' => 'growth-hacker-custom-blocks-xfeature-block-editor',
+        'editor_script' => 'growth-hacker-custom-blocks-xfeature-block',
+    ) );
+ 
+}
+add_action( 'init', 'growth_hacker_register_block' );
+
+?>
---
+++ src/index.js
@@ -0,0 +1,210 @@
+import { __ } from '@wordpress/i18n';
+import { registerBlockType } from '@wordpress/blocks';
+import { RichText, MediaUpload, URLInputButton } from '@wordpress/block-editor';
+import { Button } from '@wordpress/components';
+
+registerBlockType('growth-hacker-custom-blocks/xfeature-block', {
+    title: 'xFeature Block',
+    icon: 'smiley',
+    category: 'design',
+    attributes: {
+        content: {
+            type: 'array',
+            source: 'children',
+            selector: 'p',
+        },
+        title: {
+            type: 'array',
+            source: 'children',
+            selector: 'h2',
+        },
+        mediaID: {
+            type: 'number',
+        },
+        mediaURL: {
+            type: 'string',
+            source: 'attribute',
+            selector: 'img',
+            attribute: 'src',
+        },
+        ctaURL: {
+            type: 'string'
+        },
+        ctaLabel: {
+            type: 'string'
+        },
+        ctaMuteLabel: {
+            type: 'string'
+        },
+    },
+    example: {
+        attributes: {
+            content: __('Hello world'),
+        },
+    },
+    edit: (props) => {
+        const {
+            attributes: { title, mediaID, mediaURL, content, ctaURL, ctaLabel, ctaMuteLabel },
+            setAttributes,
+            className,
+        } = props;
+
+        const onChangeContent = (newContent) => {
+            setAttributes({ content: newContent });
+        };
+
+        const onSelectImage = (media) => {
+            setAttributes({
+                mediaURL: media.url,
+                mediaID: media.id,
+            });
+        };
+
+        const onChangeTitle = (value) => {
+            setAttributes({ title: value });
+        };
+
+        const onChangeCtaURL = (value) => {
+            setAttributes({ ctaURL: value });
+        };
+
+        const onChangeCtaLabel = (value) => {
+            setAttributes({ ctaLabel: value });
+        };
+
+        const onChangeCtaMuteLabel = (value) => {
+            setAttributes({ ctaMuteLabel: value });
+        };
+
+
+        return (
+            <div className={className}>
+                <RichText
+                    tagName="h2"
+                    placeholder={__(
+                        'Write Featured Item Title…',
+                        'growth-hacker-custom-blocks'
+                    )}
+                    value={title}
+                    onChange={onChangeTitle}
+                />
+                <div className="article-block__content">
+                    <div className="article-block__image">
+                        <MediaUpload
+                            onSelect={onSelectImage}
+                            allowedTypes="image"
+                            value={mediaID}
+                            render={({ open }) => (
+                                <Button
+                                    className={
+                                        mediaID
+                                            ? 'image-button'
+                                            : 'button button-large'
+                                    }
+                                    onClick={open}
+                                >
+                                    { !mediaID ? (
+                                        __('Upload Image', 'growth-hacker-custom-blocks')
+                                    ) : (
+                                            <img
+                                                src={mediaURL}
+                                                alt={__(
+                                                    'Upload Featured Image',
+                                                    'growth-hacker-custom-blocks'
+                                                )}
+                                            />
+                                        )}
+                                </Button>
+                            )}
+                        /></div>
+
+                    <div className="article-block__box">
+                        <div className="article-block__wrap">
+                            <div className="article-block__text">
+                                <RichText
+                                    tagName="p"
+                                    placeholder={__(
+                                        'Write Featured Item Text Here…',
+                                        'growth-hacker-custom-blocks'
+                                    )}
+                                    className={className}
+                                    onChange={onChangeContent}
+                                    value={content}
+                                />
+                            </div>
+                            <div className="article-block__footer">
+                                <RichText
+                                    className="article-block__button"
+                                    value={ctaLabel}
+                                    placeholder={__(
+                                        'Write Your CTA Label Here…',
+                                        'growth-hacker-custom-blocks'
+                                    )}
+                                    onChange={onChangeCtaLabel}
+                                />
+                                <URLInputButton className=""
+                                    url={ctaURL}
+                                    onChange={onChangeCtaURL}
+                                />
+                                <RichText
+                                    className="article-block__mute_label"
+                                    value={ctaMuteLabel}
+                                    placeholder={__(
+                                        'Pute Muted CTA Label Here…',
+                                        'growth-hacker-custom-blocks'
+                                    )}
+                                    onChange={onChangeCtaMuteLabel}
+                                />
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        );
+    },
+    save: (props) => {
+        const {
+            className,
+            attributes: { title, mediaID, mediaURL, content, ctaURL, ctaLabel, ctaMuteLabel },
+        } = props;
+
+        return (
+            <div className="{className">
+                <a className="article-block__title" href="{ctaURL}" target="_blank" rel="nofollow noopener noreferrer" ><RichText.Content tagName="h2" value={title} /></a>
+
+                <div className="article-block__content">
+                    <a className="article-block__image" href="https://www.amazon.com/dp/B07F9NGRKF/">
+
+                        {mediaURL && (
+                            <img
+                                className="article-block__image-i"
+                                src={mediaURL}
+                                alt={ctaLabel}
+                                width="270"
+                                height="270"
+                            />
+                        )}
+
+
+                    </a>
+                    <div className="article-block__box">
+                        <div className="article-block__wrap">
+                            <div className="article-block__text">
+                                <RichText.Content tagName="p" value={props.attributes.content} />
+                            </div>
+                            <div className="article-block__footer"><a className="article-block__button" href={ctaURL} target="_blank" rel="nofollow noopener noreferrer">{ctaLabel}</a>
+
+                                <span className="article-block__mute_label">{ctaMuteLabel}</span></div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        );
+    },
+});
